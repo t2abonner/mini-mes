ac78021f3d8fc6a01ce3b457628502a33fa0c197
t2abonner <t2abonner@gmail.com>
2026-09-07 22:55:55 +0300

    5.5. 🟡 Major: packages/eslint-config не объявляет собственные зависимости

--- packages/eslint-config/react.mjs
+++ packages/eslint-config/react.mjs
@@ -18,15 +18,9 @@ export const reactConfig = [
         ecmaFeatures: { jsx: true },
       },
     },
-    settings: {
-      react: {
-        version: "18.3",
-      },
-    },
     rules: {
       ...reactPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
       "react-refresh/only-export-components": "warn",
     },
   },
